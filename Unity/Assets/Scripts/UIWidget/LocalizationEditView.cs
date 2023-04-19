f392fad426217a69a2d47b323349476615e04360
ZhangHuan <2360950578@qq.com>
2023-04-19 17:05:25 +0800

    add commander model edit dialog

---
+++ Unity/Assets/Scripts/UIWidget/LocalizationEditView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Game.Model;
+using UnityEngine;
+using UnityEngine.UI;
+using Table;
+
+namespace Game.UI
+{
+    public class LocalizationEditView : MonoBehaviour
+    {
+        public CommanderContentDialog CommanderContentDialog { get; set; }
+        private CommanderModel m_CommanderModel;
+
+        public void SetCommanderModel(CommanderModel model)
+        {
+            m_CommanderModel = model;
+        }
+    }
+}
