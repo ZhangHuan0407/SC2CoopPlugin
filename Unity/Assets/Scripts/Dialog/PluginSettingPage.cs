08d60f0df76dddadcdb678c4b6388fe7e2e38fee
Zhang Huan <2360950578@qq.com>
2023-04-29 17:07:34 +0800

    update setting dialog

--- Unity/Assets/Scripts/Dialog/PluginSettingPage.cs
+++ Unity/Assets/Scripts/Dialog/PluginSettingPage.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Table;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,6 +8,15 @@ namespace Game.UI
 {
     public class PluginSettingPage : SettingPage
     {
+        private List<(string languageName, SystemLanguage language)> SystemLanguageList = new List<(string, SystemLanguage)>()
+        {
+            (null, SystemLanguage.ChineseSimplified),
+            (null, SystemLanguage.ChineseTraditional),
+            (null, SystemLanguage.English),
+            (null, SystemLanguage.French),
+            (null, SystemLanguage.German),
+            (null, SystemLanguage.Korean),
+        };
         [SerializeField]
         private Dropdown m_InterfaceLanguage;
         [SerializeField]
@@ -17,13 +27,20 @@ namespace Game.UI
         private void Start()
         {
             m_InGameLanguageDropDown.SetTextWithoutNotify(SettingDialog.UserSetting.InGameLanguage);
-            string usedLanguage = SettingDialog.UserSetting.InterfaceLanguage.ToString();
-            m_InterfaceLanguage.SetValueWithoutNotify(m_InterfaceLanguage.options.FindIndex(data => data.text == usedLanguage));
+            for (int i = 0; i < SystemLanguageList.Count; i++)
+            {
+                string languageName = TableManager.LocalizationTable[SystemLanguageList[i].language];
+                SystemLanguageList[i] = (languageName, SystemLanguageList[i].language);
+            }
+            m_InterfaceLanguage.ClearOptions();
+            for (int i = 0;i < SystemLanguageList.Count; i++)
+                m_InterfaceLanguage.options.Add(new Dropdown.OptionData(SystemLanguageList[i].languageName));
+            SystemLanguage usedLanguage = SettingDialog.UserSetting.InterfaceLanguage;
+            m_InterfaceLanguage.SetValueWithoutNotify(SystemLanguageList.FindIndex(data => data.language == usedLanguage));
             m_InterfaceLanguage.onValueChanged.AddListener((int index) =>
             {
-                SystemLanguage language = (SystemLanguage)Enum.Parse(typeof(SystemLanguage), m_InterfaceLanguage.options[index].text);
-                SettingDialog.UserSetting.InterfaceLanguage = language;
-                TableManager.LoadLocalizationTable(language);
+                SettingDialog.UserSetting.InterfaceLanguage = SystemLanguageList[index].language;
+                TableManager.LoadLocalizationTable(SystemLanguageList[index].language);
             });
             m_IsProgrammer.SetIsOnWithoutNotify(SettingDialog.UserSetting.IsProgrammer);
             m_IsProgrammer.onValueChanged.AddListener((bool value) =>
